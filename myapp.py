86ec7555501bce757527eb5737cc8a8a4f20e89c
Nathan attoh <nnaattoh@st.ug.edu.gh>
2025-09-25 12:51:36 +0000

    added about route and simple page

--- myapp.py
+++ myapp.py
@@ -12,6 +12,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def index():
+    return render_template('about.html')
+
+
 # @app.route('/whereami')
 # def whereami():
 #     return 'Ghana!'
